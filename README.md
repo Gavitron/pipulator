9b521149ae021762414385e56448b534f9230b20
Gavin McDonald <gmcdonald@ea.com>
2015-11-13 18:48:45 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+Pipulator
+---------
+Emulate the Bethesda Pipboy Protocol as either the App or the fallout4 game instance.
+
+Usage
+-----
+Built by reverse engineering comms between my Android App and PC game.
+
+ - cd <this directory>
+ - python handshake.py &
+ - Open android app, Connection Setup -> PC 
+ -  select your pipulator from the list
+
+Requirements
+------------
+ - Python
+ - The android app
+ - Fallout4 for PC
+
+ ** you might be able to get this working with iOS and an Xbox/PS4, but I don't have either, so I can't say with any certainty.  I do know that smartglass works differently than a PC server would.
+
+Known Bugs
+----------
+ - not complete
+ - this is a hack.
+
+FAQ
+---
+ ** None, cause nobody knows this exists yet.
---
+++ udp_handshake.py
@@ -0,0 +1,42 @@
+#  udp listener
+#
+#  Run this in the background, and you will appear to the pipboy app as an instance of Fallout4
+
+import socket
+import struct
+import sys
+
+multicast_group = '224.3.29.71'
+server_address = ('', 28000)
+
+msg_ping = '{"cmd":"autodiscover"}'
+msg_ack  = '{ "IsBusy" : false, "MachineType" : "PC" }'
+
+# Create the socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind to the server address
+sock.bind(server_address)
+
+# Set the time-to-live for messages to 1 so they do not go past the
+# local network segment.
+ttl = struct.pack('b', 127)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+
+# Tell the operating system to add the socket to the multicast group
+# on all interfaces.
+group = socket.inet_aton(multicast_group)
+mreq = struct.pack('4sL', group, socket.INADDR_ANY)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+# Receive/respond loop
+while True:
+    print >>sys.stderr, '\nwaiting to receive message'
+    data, address = sock.recvfrom(1024)
+
+    print >>sys.stderr, 'received %s bytes from %s' % (len(data), address)
+    print >>sys.stderr, data
+
+    print >>sys.stderr, 'sending acknowledgement to', address
+    sock.sendto(msg_ack, address)
+    #sock.sendto('ack', address)
