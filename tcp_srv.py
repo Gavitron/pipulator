ac7dcad840e66d2884c4d2622850981569801bc2
Gavin McDonald <gmcdonald@ea.com>
2015-11-13 21:16:26 -0800

    some more mangling, and a bugfix.

--- tcp_srv.py
+++ tcp_srv.py
@@ -12,6 +12,15 @@ sock.bind(server_address)
 # Listen for incoming connections
 sock.listen(1)
 
+replies = {
+    'nullstr' : '\x00\x00\x00\x00\x00',
+    'onestr' : '\x00\x0f\x00\x00\x00\x00',
+    'fivstr' : '\x05\x9dr\x01\x00\x10\xef\x82A',
+    'sixstr' : '\x06\x00\x00\x00\x03',
+    'ninstr' : '\x09\x00\x00\x00\x03',
+    'twvstr' : '\x12\x00\x00\x00\x03',
+}
+
 with open('repl.txt', 'r') as content_file:
     content = content_file.read()
 
@@ -25,19 +34,19 @@ while True:
 
         # once we crack the data format, this would be a 'hydrate data' moment
         has_sent_gestalt = False
+        key = 'nullstr'
 
-        # Receive the data in small chunks and display it
+        # Receive the data in chunks and display it, wihle keeping the heartbeat going
         while True:
             data = connection.recv(1024)
             print >>sys.stderr, 'received "%r"' % data
             if data:
                 if has_sent_gestalt:
-                    print >>sys.stderr, 'sending nullstr'
-                    connection.sendall('\x00\x00\x00\x00\x00') 
+                    print >>sys.stderr, 'sending %s' % key
+                    connection.sendall(replies[key]) 
                 else:
                     has_sent_gestalt = True
                     print >>sys.stderr, 'sending gestalt'
-                    f = open('repl.txt')
                     connection.sendall(content)
 
             else:
